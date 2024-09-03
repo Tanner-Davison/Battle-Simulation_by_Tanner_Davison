2c0940c259d5d1b3b322bba91343072d58175fd2
Tanner Davison <tanner.davison95@gmail.com>
2024-09-02 23:22:21 -0600

    fixed and converted to cmake project

---
+++ build/CMakeFiles/Cplusplus_Project.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/tanner.davison/Desktop/Cplusplus_Project/HelloWorldDemo/CharacterFuncs.cpp" "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/CharacterFuncs.cpp.o" "gcc" "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/CharacterFuncs.cpp.o.d"
+  "/Users/tanner.davison/Desktop/Cplusplus_Project/HelloWorldDemo/MathFuncs.cpp" "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/MathFuncs.cpp.o" "gcc" "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/MathFuncs.cpp.o.d"
+  "/Users/tanner.davison/Desktop/Cplusplus_Project/HelloWorldDemo/main.cpp" "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/main.cpp.o" "gcc" "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Cplusplus_Project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/CharacterFuncs.cpp.o"
+  "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/CharacterFuncs.cpp.o.d"
+  "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/MathFuncs.cpp.o"
+  "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/MathFuncs.cpp.o.d"
+  "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/main.cpp.o"
+  "CMakeFiles/Cplusplus_Project.dir/HelloWorldDemo/main.cpp.o.d"
+  "Cplusplus_Project"
+  "Cplusplus_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cplusplus_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
